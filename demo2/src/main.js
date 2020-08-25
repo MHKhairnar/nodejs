78c7c3d69d40e21f7b46e35ffe107d7da18f5350
MHKhairnar <65165238+MHKhairnar@users.noreply.github.com>
2020-08-25 15:57:51 +0530

    Add files via upload

---
+++ demo2/src/main.js
@@ -0,0 +1,21 @@
+//console.log("mahesh");
+
+const mod1 = require("./my.mod/my.module1");
+const mod2 = require("./my.mod/my.module2");
+const mod3 = require("./my.mod/my.module3");
+const mod4 = require("./my.mod/my.module4");
+
+// log from module 1
+// console.log(mod1);
+
+// log from module 2
+//const total = mod1.sum(10, 20);
+// console.log(total);
+//console.log(mod2);
+
+// log from module 3
+//const sumTotal = mod3.sum(1, 2);
+//console.log("sum", sumTotal);
+
+// log from module 4
+console.log(mod4);
---
+++ demo2/src/my.mod/my.module2.js
@@ -0,0 +1,27 @@
+const NUM = 1234;
+const BOOL = false;
+const str = "mahesh";
+
+const MY_ARR = [1, 2, 3, 4];
+const MY_ARR_STR = ["mahesh", "pratik", "umesh"];
+
+const myjson = {
+  id: 100,
+  title: "MUMBAI",
+};
+const list = [
+  { id: 1, title: "A" },
+  { id: 1, title: "B" },
+  { id: 1, title: "C" },
+];
+module.exports = {
+  NUM,
+  BOOL,
+  str,
+
+  MY_ARR,
+  MY_ARR_STR,
+
+  myjson,
+  list,
+};
